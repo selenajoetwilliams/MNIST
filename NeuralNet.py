65df980b64135bfb8cd60756fbf2e287c89f9330
selenajoetwilliams <selena_williams@brown.edu>
2023-01-08 12:43:41 -0800

    builing the network onto model parameters

--- NeuralNet.py
+++ NeuralNet.py
@@ -92,6 +92,51 @@ class NeuralNetwork(nn.Module):
 model = NeuralNetwork().to(device)
 print(model)
 
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X) 
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+# Model Layers
+
+# sampling a mini batch to see what happens as it goes through the model
+input_image = torch.rand(3, 28, 28)
+print(input_image.size()) 
+
+# flattening the dimensions from 28x28 2D array to a 1D array of 784 pixels
+flatten = nn.Flatten() 
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+# linear layer
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+# relu layer 
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}") 
+# question: is the hidden layer meant to improve the assumptions, as seen in smaller abs vals of the values in each index of the tensor?
+
+# sequential layer
+# TODO: look up seq_modules in pytorch documentation
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20,10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+
+# softmax layer 
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
+
+
+
 
 
 # question: these tensors are blank right? 
