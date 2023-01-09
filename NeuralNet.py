c510aea3060b983162a3f52ba3736a7c80cf516e
selenajoetwilliams <selena_williams@brown.edu>
2023-01-09 12:54:59 -0800

    victory

--- NeuralNet.py
+++ NeuralNet.py
@@ -11,6 +11,9 @@ from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
+###########################################
+# TENSORS
+
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -24,6 +27,9 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
 
 labels_map = {
     0: "T-Shirt",
@@ -90,6 +96,7 @@ class NeuralNetwork(nn.Module):
         return logits
 
 model = NeuralNetwork().to(device)
+# should the line above be: model = NeuralNetwork()
 print(model)
 
 X = torch.rand(1, 28, 28, device=device)
@@ -172,8 +179,72 @@ print(b.grad)
  I don't think I need for the MNIST neural network
 """
 
+###########################################
+# OPTIMIZING MODEL PARAMETERS
 
+"""
+Note that the following lines were added in the tensors section in order to 
+have all the right code for optimization:
+    train_dataloader = DataLoader(training_data, batch_size=64)
+    test_dataloader = DataLoader(test_data, batch_size=64)
+"""
 
+# question: at this step when I run the program, my output doesn't match the
+# output in the tutorial 
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# full implementation of optimization algorithm
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction & loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            # question: what do the :>7f, :>5d mean in the line above?
+
+# defining the training & test loop
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# initializing the loss function optimizer
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n----------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
 
 # question: these tensors are blank right? 
 # question: what about loading the acutal data?
